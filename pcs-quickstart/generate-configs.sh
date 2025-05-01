d338ebce2570c42222ce3c4b30ec1c9de963dc37
Ben McDonald <46734217+bmcdonald3@users.noreply.github.com>
2025-05-01 11:04:27 -0700

    Switch to 8444

--- pcs-quickstart/generate-configs.sh
+++ pcs-quickstart/generate-configs.sh
@@ -81,7 +81,7 @@ configs/opaal-template.yaml > configs/opaal.yaml
 
 # Generate CoreDHCP configuration from configs/coredhcp-template.yaml.
 sed \
-  -e "s|<BASE_URL>|https://${SYSNAME}.${SYSDOMAIN}:8443|g" \
+  -e "s|<BASE_URL>|https://${SYSNAME}.${SYSDOMAIN}:8444|g" \
   configs/coredhcp-template.yaml > configs/coredhcp.yaml
 
 # Set the system name
